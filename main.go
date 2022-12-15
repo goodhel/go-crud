880533cf107a062b4debd7c4b27ef4580548628e
yudhie123 <yudhie123@gmail.com>
2022-12-15 09:05:10 +0700

    refactor: routes api

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/goodhel/go-crud/controllers"
 	"github.com/goodhel/go-crud/initializers"
-	"github.com/goodhel/go-crud/middleware"
+	"github.com/goodhel/go-crud/routes"
 )
 
 func init() {
@@ -13,23 +12,13 @@ func init() {
 }
 
 func main() {
-	r := gin.Default()
+	router := routes.Routes()
 
-	r.GET("/", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello this is api from Backend Go",
 		})
 	})
 
-	r.GET("/posts", controllers.ListPosts)
-	r.GET("/posts/:id", controllers.DetailPosts)
-	r.POST("/posts", controllers.PostsCreate)
-	r.PUT("/posts/:id", controllers.UpdatePost)
-	r.DELETE("/posts", controllers.DeletePost)
-	r.POST("/register", controllers.Register)
-	r.POST("/login", controllers.Login)
-	r.GET("/validate", middleware.UserSession, controllers.Validate)
-	r.GET("/users", middleware.UserSession, controllers.ListUsers)
-
-	r.Run() // listen and serve on 0.0.0.0:8081
+	router.Run() // listen and serve on 0.0.0.0:8081
 }
---
+++ routes/routes.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/goodhel/go-crud/controllers"
+	"github.com/goodhel/go-crud/middleware"
+)
+
+func Routes() *gin.Engine {
+	router := gin.Default()
+
+	router.POST("/register", controllers.Register)
+	router.POST("/login", controllers.Login)
+
+	api := router.Group("/api")
+	{
+		api.GET("/posts", controllers.ListPosts)
+		api.GET("/posts/:id", controllers.DetailPosts)
+		api.POST("/posts", controllers.PostsCreate)
+		api.PUT("/posts/:id", controllers.UpdatePost)
+		api.DELETE("/posts", controllers.DeletePost)
+		api.GET("/validate", middleware.UserSession, controllers.Validate)
+		api.GET("/users", middleware.UserSession, controllers.ListUsers)
+	}
+
+	return router
+}
